4b1ba72a702bfa95c6a4944e2f0bafe73dbcdf0a
JIVESH <bravojivesh@gmail.com>
2023-12-25 00:10:28 +0530

    All cases done. Two business logic. Brain= mine, Brain2= instructor's

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from question_model import Question
 from data import question_data
+# from quiz_brain import QuizBrain
+from question_brain2 import QuizBrain
+#JH: Either import brain2 or brain. They are variations.
 
 length= len(question_data)
 # print (length)
@@ -9,10 +12,17 @@ question_bank=[]
 for i in range(length):
     question= question_data[i]["text"] #extracting the question part only
     answer= question_data[i]["answer"] #extracting the answer part only
-    qna=Question(question,answer) #a new variable to store Question object. Note this changes
-    #with every value of i. So we need to store all the values. Look below.
+    qna=Question(question,answer) #a new variable to store Question object.
+    # Note: this is only one record.This changes with every value of i.
+    # So we need to store all the values. Look below.
     question_bank.append(qna) #cont: so we need a list to store all the objects.
 
-print (question_bank[11].text)
+# print (question_bank[11].text)
 
+q_list= QuizBrain(question_bank)
 
+while q_list.still_has_questions():
+    q_list.next_question()
+    # q_list.count_score() #Only Needed with brain (not with brain 2)
+
+print (f"final score is: {q_list.score}/{q_list.q_num}")
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,4 +1,31 @@
 class QuizBrain:
     def __init__(self, q_list):
         self.q_num=0
-        
+        self.question_list= q_list
+        self.user_answer= ""
+        self.right_answer=""
+        self.score= 0
+
+    def still_has_questions(self):
+        length= len (self.question_list)
+        return self.q_num < length
+        # the above is same as saying "if condition return True else return False"
+
+    def next_question(self):
+        current_question= self.question_list[self.q_num]
+        # ask= input (f"{current_question["text"]} True/False: ")
+        # JH: above is how I wrote earlier. The "text" part would be WROng.
+        # Reason: question_list WILL BE question bank with a list of "Question class objects"
+        # So we need to write "current_question.text" (following Question class attributes.
+        ask = input(f"Q{(self.q_num) +1}. {current_question.text} True/False: ")
+        self.user_answer= ask
+        self.right_answer= current_question.answer
+        self.q_num += 1
+
+
+    def count_score(self):
+        if (self.user_answer).lower()== (self.right_answer).lower():
+            self.score+=1
+            print (f" Correct! Your current score is {self.score}/{self.q_num} ")
+        else:
+            print (f" Wrong! Your current score is {self.score}/{self.q_num} ")
