6b38024c5d7712e92a2495e7a3f6a4d1e40cf8b9
JIVESH HAMAL <bravojivesh@gmail.com>
2023-12-25 14:28:19 +0530

    added external data. Changed key names. Same business logic handles the new data. Benefit of OOP

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from question_model import Question
-from data import question_data
+
+# from data import question_data
+from data2 import question_data
+#JH: Either import data or data2. They are variations.
+
 # from quiz_brain import QuizBrain
 from question_brain2 import QuizBrain
 #JH: Either import brain2 or brain. They are variations.
@@ -10,8 +14,8 @@ length= len(question_data)
 question_bank=[]
 
 for i in range(length):
-    question= question_data[i]["text"] #extracting the question part only
-    answer= question_data[i]["answer"] #extracting the answer part only
+    question= question_data[i]["question"] #extracting the question part only
+    answer= question_data[i]["correct_answer"] #extracting the answer part only
     qna=Question(question,answer) #a new variable to store Question object.
     # Note: this is only one record.This changes with every value of i.
     # So we need to store all the values. Look below.
@@ -23,6 +27,7 @@ q_list= QuizBrain(question_bank)
 
 while q_list.still_has_questions():
     q_list.next_question()
+    print () #this is for printing a blank line between questions.
     # q_list.count_score() #Only Needed with brain (not with brain 2)
 
 print (f"final score is: {q_list.score}/{q_list.q_num}")
